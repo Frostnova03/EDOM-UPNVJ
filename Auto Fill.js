99befba597788371fd8b94d2c2c1af4a4fd28c79
Frostnova03 <95489792+Frostnova03@users.noreply.github.com>
2021-12-03 22:13:07 +0700

    Add files via upload

---
+++ Auto Fill.js
@@ -0,0 +1,139 @@
+var elements = document.getElementsByName('rb1');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb2');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb3');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb4');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb5');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb6');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb7');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb8');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb9');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb10');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb11');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb12');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb13');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb14');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb15');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb16');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb17');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb18');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb19');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
+
+var elements = document.getElementsByName('rb20');
+for (i = 0; i < elements.length; i++) {
+    if (elements[i].value == (Math.floor(Math.random() * 2) + 3)){
+        elements[i].checked = true;
+    }
+}
